a44816470ed6bfe36340a26c9ec7b85b1544ef7b
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-09-10 20:04:58 +0600

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace GarbageCollectorLesson
+{
+    class Program
+    {
+        private static long milliseconds = 10000000;
+        static void Main(string[] args)
+        {
+            int number = 5;
+            bool isError = false;
+            char a = 'a';
+            // структуры значимые типы живут в области видимости { } 
+            // стек => значимые типы(типы значений) int, long, char != NULL нельзя присвоить NULL
+
+            var user = new User
+            {
+                Login = "admin",
+                Password = "12345"
+            };
+
+            for (var i = 0;i < 100000;i++)
+            {
+                GC.Collect();
+                Thread.Sleep(1000);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarbageCollectorLesson
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public DateTime? BlockedDate { get; set; }
+    }
+}
